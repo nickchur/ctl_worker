f59155f05519333095de04f4df705c42975fa977
nickchur <sber@DESKTOP-LTS6DVR>
2026-04-30 19:14:12 +0000

    docs: clarify condition parameter description

--- er_export/er_export.py
+++ er_export/er_export.py
@@ -162,7 +162,7 @@ for table_key, params in wfs.items():
         render_template_as_native_obj=True,
         params={
             'extract_time':    Param(None, type=['string', 'null'],  title='Extract time',       description='Перезаписать: 2024-01-01 00:00:00'),
-            'condition':       Param(None, type=['string', 'null'],  title='Condition',           description='Перезаписать SQL-условие WHERE'),
+            'condition':       Param(None, type=['string', 'null'],  title='Condition',           description='Перезаписать значение плейсхолдера {condition}'),
             'is_current':      Param(None, type=['boolean', 'null'], title='Is current'),
             'increment':       Param(params.get('increment'), type=['integer', 'null'], title='Increment (сек)'),
             'selfrun_timeout': Param(params.get('selfrun_timeout'), type=['integer', 'null'], title='Selfrun timeout (мин)'),
